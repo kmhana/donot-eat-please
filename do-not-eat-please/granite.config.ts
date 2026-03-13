6e9bfe35b144b094c7701484597b742f25608b58
demian.hana <dlakrb@naver.com>
2026-03-13 15:54:03 +0900

    [DEP-2] img link

--- do-not-eat-please/granite.config.ts
+++ do-not-eat-please/granite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       brand: {
         displayName: 'do-not-eat-please', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
         primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-        icon: '', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+        icon: 'https://github.com/kmhana/donot-eat-please/blob/main/do-not-eat-please/img/light.png', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
       },
       permissions: [],
     }),
